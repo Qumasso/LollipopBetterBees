86fda812ecc0ff0ac53a0e5ca824dd640d624798
Qumasso <qumasso@gmail.com>
2022-10-20 18:08:49 +0300

    Some config garbage was removed from resources folder Not needed lines were deleted

--- src/main/java/net/lollipopmc/lollipopbetterbees/BetterBeesPlugin.java
+++ src/main/java/net/lollipopmc/lollipopbetterbees/BetterBeesPlugin.java
@@ -5,8 +5,6 @@ import net.lollipopmc.lollipopbetterbees.config.ConfigWorker;
 import net.lollipopmc.lollipopbetterbees.config.HiveConfigHolder;
 import net.lollipopmc.lollipopbetterbees.config.NestConfigHolder;
 import net.lollipopmc.lollipopbetterbees.events.EventListener;
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -31,28 +29,10 @@ public class BetterBeesPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(handler, this);
     }
 
-/*
-    public static void main(String[] args) {
-        System.out.println(ChatColor.translateAlternateColorCodes('&', "&x&f&f&e&e&f&f") + " §");
-    }
-
-
- */
-
     private void initConfigs() {
-        beeConfig = new BeeConfigHolder(new File(getDataFolder() + File.separator + "bees.yml"));
-        Bukkit.broadcastMessage(beeConfig.toString());
-        beeConfig.load();
-        hiveConfig = new HiveConfigHolder(new File(getDataFolder() + File.separator + "hive.yml"));
-        hiveConfig.load();
-        nestConfig = new NestConfigHolder(new File(getDataFolder() + File.separator + "nest.yml"));
-        nestConfig.load();
-        //saveDefaultConfig();
-        /*
-        saveResource(ConfigWorker.HIVE_CONFIG_NAME, true);
-        saveResource(ConfigWorker.NEST_CONFIG_NAME, true);
-
-         */
+        beeConfig = new BeeConfigHolder(new File(getDataFolder(), "bees.yml")).load();
+        hiveConfig = new HiveConfigHolder(new File(getDataFolder(), "hive.yml")).load();
+        nestConfig = new NestConfigHolder(new File(getDataFolder(), "nest.yml")).load();
     }
 
 }
